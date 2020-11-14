85f80dcdbed93ba3949718d4d2b570299b23e0df
Whitney Harden <wtharden320@gmail.com>
2020-11-13 23:29:38 -0500

    delete and detroy function for delete sequelize request

--- server/controllers/logcontroller.js
+++ server/controllers/logcontroller.js
@@ -53,5 +53,30 @@ router.get('/:id', validateSession, (req, res) => {
     .catch(err => res.status(500).json({ error: err }));
 });
 
+router.put('/:id', validateSession, (req, res) => {
+  const updateLog = {
+    description: req.body.log.description,
+    definition: req.body.log.definition,
+    result: req.body.log.result,
+    owner_id: req.user.id
+  };
+
+  const query = { where: {id: req.params.id} };
+  console.log('req.params.id=====> ', req.params.id);
+
+  Log.update(updateLog, query)
+    .then(log => res.status(200).json(log))
+    .catch(err => res.status(500).json({ error: err }));
+});
+
+//Delete
+router.delete('/:id', validateSession, (req, res) => {
+  const query = { where: {id: req.params.id} };
+
+  Log.destroy(query)
+    .then(() => res.status(200).json({message: 'Log Deleted'}))
+    .catch(err => res.status(500).json({ error: err }));
+});
+
 
 module.exports = router;
